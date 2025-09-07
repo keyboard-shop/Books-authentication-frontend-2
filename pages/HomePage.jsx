bc7e9b4b92788681f6f8c08a46fdcf5cf0807071
keyboard <jom184750@gmail.com>
2025-09-07 11:03:54 +0300

    Books Auth-4444444444444444

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
     <div className='home-page'>
       <h2>Authentication</h2>
       <h4>
-        Books Auth-2
+        Books Auth-4444444444444444
       </h4>
     </div>
   )
