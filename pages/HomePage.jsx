268cdeff228cf0d7ca130baf3a56c18726fec552
keyboard <jom184750@gmail.com>
2025-09-07 10:36:22 +0300

    h2 h4

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -7,12 +7,9 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className='home-page'>
-      <h2>Hello 777!</h2>
+      <h2>Authentication</h2>
       <h4>
-      If you are reading this message, it means that I have managed to deploy this website on the Internet.
-       I deployed both the frontend and backend folders on Vercel.
-        I was unable to deploy the backend on Render, as it always asked for my payment card, even though I selected the free plan:
-        select free plan - deploy - payment card.
+        Books Auth
       </h4>
       <br />
       <h4>
